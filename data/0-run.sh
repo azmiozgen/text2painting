a24ce1e9a7aa4de863cd832d61045fd1878dc6c8
Azmi Ozgen <ozgenaz@itu.edu.tr>
2019-11-13 18:46:54 +0300

    Uncomment lines

--- data/0-run.sh
+++ data/0-run.sh
@@ -1,16 +1,16 @@
 echo
 echo    Subsets are being united..
-#python 1-unite_labels.py
+python 1-unite_labels.py
 
 echo
 echo    Train-val-test are being splitted..
-#python 2-split_labels.py
+python 2-split_labels.py
 
 echo
 echo    Word2Vec started..
-#python 3-word2vec.py united/train_labels.csv united
+python 3-word2vec.py united/train_labels.csv united
 
 echo
 echo    Image stats are being extracted..
-#python 4-get_image_stats.py united train_labels.csv train
+python 4-get_image_stats.py united train_labels.csv train
 python 4-get_image_stats.py united val_labels.csv val
