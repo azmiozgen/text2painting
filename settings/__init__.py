032e755dd597528c39eb6d94d3313452a5aa58ca
Azmi Ozgen <ozgenaz@itu.edu.tr>
2019-11-19 13:09:56 +0300

    Delete settings/ dir

--- settings/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data_settings import DataSettings
-from .model_settings import ModelSettings
-from .training_settings import TrainingSettings
-from .pred_settings import PredSettings
--- settings/pred_settings.py
+++
@@ -1,6 +0,0 @@
-
-class PredSettings(object):
-
-    def __init__(self):
-
-        self.LATIN_CONF_THRESHOLD = 0.5
