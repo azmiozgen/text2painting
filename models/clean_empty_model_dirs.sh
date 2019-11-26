dc83fe1eef26647a1f3706d26de1076b45b4be69
Azmi Ozgen <ozgenaz@itu.edu.tr>
2019-11-26 12:55:06 +0300

    Add models/ scripts after fixing .gitignore

---
+++ models/clean_empty_model_dirs.sh
@@ -0,0 +1,10 @@
+model_name=united
+for d in ${model_name}*
+do
+    count_pth=`ls ${d}/*pth 2> /dev/null | wc -l`
+    if [[ $count_pth == 0 ]]
+    then
+        ls -d $d
+        rm -r $d
+    fi
+done
