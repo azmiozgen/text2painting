Fix accuracy calculation, add pixel discriminator

lib/arch.py
@@ -175,6 +175,33 @@ class GeneratorStackGAN1(nn.Module):
 
         return out
 
+
+class DiscriminatorPixel(nn.Module):
+
+    def __init__(self, config):
+        super(DiscriminatorPixel, self).__init__()
+
+        ndf = config.NDF
+        n_channel = config.N_CHANNELS + 1   ## Stitching images and word vectors
+        norm_layer = config.NORM_LAYER
+
+        if type(norm_layer) == functools.partial:  # no need to use bias as BatchNorm2d has affine parameters
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+
+        self.net = nn.Sequential(
+                                nn.Conv2d(n_channel, ndf, kernel_size=1, stride=1, padding=0),
+                                nn.LeakyReLU(0.2, True),
+                                nn.Conv2d(ndf, ndf * 2, kernel_size=1, stride=1, padding=0, bias=use_bias),
+                                norm_layer(ndf * 2),
+                                nn.LeakyReLU(0.2, True),
+                                nn.Conv2d(ndf * 2, 1, kernel_size=1, stride=1, padding=0, bias=use_bias)
+                                )
+
+    def forward(self, x):
+        return self.net(x)
+
 ### DO NOT USE BatchNorm in D if WGANGP is used as loss function ###
 class DiscriminatorStackGAN1(nn.Module):
     def __init__(self, config):
@@ -185,10 +212,10 @@ class DiscriminatorStackGAN1(nn.Module):
         self.ndf = ndf
         self.ngf = ngf
 
-        self.conv = nn.Sequential(                                                        # (4) x H x W
-                                 nn.Conv2d(n_channel, ndf, 4, 2, 1, bias=False),    # (ndf) x H/2 x W/2
+        self.conv = nn.Sequential(                                                    # (4) x H x W
+                                 nn.Conv2d(n_channel, ndf, 4, 2, 1, bias=False),      # (ndf) x H/2 x W/2
                                  nn.LeakyReLU(0.2, inplace=True),
-                                 nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),      # (ndf * 2) x H/4 x W/4
+                                 nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),        # (ndf * 2) x H/4 x W/4
                                  nn.BatchNorm2d(ndf * 2),
                                 #  nn.InstanceNorm2d(ndf * 2),
                                  nn.LeakyReLU(0.2, inplace=True),

lib/config.py
@@ -58,7 +58,7 @@ class Config():
         self.N_INPUT = self.SENTENCE_LENGTH * self.WV_SIZE
         self.NGF = 128
         self.NDF = 128
-        self.GAN_LOSS = 'wgangp'   ## One of 'lsgan', 'vanilla', 'wgangp'
+        self.GAN_LOSS = 'lsgan'   ## One of 'lsgan', 'vanilla', 'wgangp'
         self.LAMBDA_L1 = 1.0
         self.NORM_LAYER = torch.nn.BatchNorm2d
         self.USE_DROPOUT = True

lib/model.py
@@ -8,7 +8,7 @@ from PIL import Image
 import torch
 from torchvision.utils import make_grid
 
-from .arch import GeneratorResNet, DiscriminatorStackGAN1
+from .arch import GeneratorResNet, DiscriminatorPixel
 from .utils import GANLoss, get_gradient_penalty, get_uuid, words2image, ImageUtilities
 
 class BaseModel(ABC):
@@ -81,13 +81,12 @@ class BaseModel(ABC):
 
 class GANModel(BaseModel):
 
-    def __init__(self, config, model_file=None, mode='train', reset_lr=False, accuracy=False):
+    def __init__(self, config, model_file=None, mode='train', reset_lr=False):
 
         assert mode in ['train', 'test'], 'Mode should be one of "train, test"'
         self.config = config
         self.mode = mode
         self.reset_lr = reset_lr
-        self.accuracy = accuracy
         self.device = config.DEVICE
         self.model_name = config.MODEL_NAME
         self.log_header = config.LOG_HEADER
@@ -104,9 +103,10 @@ class GANModel(BaseModel):
 
         ## Init networks and optimizers
         if mode == 'train':
-            self.D = DiscriminatorStackGAN1(config).to(self.device)
+            self.D = DiscriminatorPixel(config).to(self.device)
 
-            self.criterionGAN = GANLoss(self.gan_loss, self.device, accuracy=accuracy).to(self.device)
+            self.G_criterionGAN = GANLoss(self.gan_loss, self.device, accuracy=False).to(self.device)
+            self.D_criterionGAN = GANLoss(self.gan_loss, self.device, accuracy=True).to(self.device)
             self.criterionL1 = torch.nn.L1Loss()
             self.G_optimizer = torch.optim.Adam(self.G.parameters(),
                                                 lr=lr,
@@ -266,15 +266,15 @@ class GANModel(BaseModel):
     def backward_D(self, rr_pair, rf_pair, fr_pair, update=True):
         # Real-real
         pred_rr = self.D(rr_pair)
-        self.loss_D_rr, self.accuracy_D_rr = self.criterionGAN(pred_rr, target_is_real=True)
+        self.loss_D_rr, self.accuracy_D_rr = self.D_criterionGAN(pred_rr, target_is_real=True)
 
         ## Real-fake
         pred_rf = self.D(rf_pair)
-        self.loss_D_rf, self.accuracy_D_rf = self.criterionGAN(pred_rf, target_is_real=False)
+        self.loss_D_rf, self.accuracy_D_rf = self.D_criterionGAN(pred_rf, target_is_real=False)
 
         ## Fake-real
         pred_fr = self.D(fr_pair.detach())
-        self.loss_D_fr, self.accuracy_D_fr = self.criterionGAN(pred_fr, target_is_real=False)
+        self.loss_D_fr, self.accuracy_D_fr = self.D_criterionGAN(pred_fr, target_is_real=False)
 
         if self.gan_loss == 'wgangp':
             self.loss_gp_fr, _ = get_gradient_penalty(self.D, rr_pair, fr_pair.detach(), self.device,
@@ -292,7 +292,7 @@ class GANModel(BaseModel):
         ## Fake-real
         pred_fr = self.D(fr_pair.detach())
 
-        loss_G_GAN, _ = self.criterionGAN(pred_fr, target_is_real=True)
+        loss_G_GAN, _ = self.G_criterionGAN(pred_fr, target_is_real=True)
         loss_G_L1 = self.criterionL1(fake_images_tensor, real_images_tensor) * self.lambda_l1
 
         self.loss_G = loss_G_GAN + loss_G_L1

lib/utils.py
@@ -55,15 +55,15 @@ class GANLoss(nn.Module):
 
         ## Compute accuracy
         if self.accuracy:
-            _prediction = prediction.detach().cpu().numpy()
-            _target_tensor = target_tensor.detach().cpu().numpy()
+            _prediction = prediction.detach().cpu()
+            _target_tensor = target_tensor.detach().cpu()
             if target_is_real:
-                accuracy = np.mean(np.argmax(_prediction, axis=1) == _target_tensor)
+                accuracy = torch.mean(((torch.sigmoid(_prediction) >= 0.5).float() == _target_tensor).float()).item()
             else:
-                accuracy = np.mean(np.argmax(_prediction, axis=1) == _target_tensor)
+                accuracy = torch.mean(((torch.sigmoid(_prediction) < 0.5).float() == _target_tensor).float()).item()
             return loss, accuracy
 
-        return loss, 0.0
+        return loss, -1.0
 
 def get_gradient_penalty(netD, real_data, fake_data, device, type='mixed', constant=1.0, lambda_gp=10.0):
     """Calculate the gradient penalty loss, used in WGAN-GP paper https://arxiv.org/abs/1704.00028